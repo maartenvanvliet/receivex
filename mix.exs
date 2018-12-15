3329581d34670b77d7ca098f972285e16abc49a5
Maarten van Vliet <maartenvanvliet@gmail.com>
2018-12-15 13:34:54 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Receivex.MixProject do
   def project do
     [
       app: :receivex,
-      version: "0.8.0",
+      version: "0.8.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
