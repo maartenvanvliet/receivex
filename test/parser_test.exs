a4640bb4b26aecfb3c3a03d3f3c9f9d6b74df1dd
Chase Pursley <chasepursley@gmail.com>
2021-12-21 16:10:57 -0500

    Make credo happy

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -4,7 +4,7 @@ defmodule Receivex.Parser.Test do
 
   test "parse_timestamp/1: returns timestamp string" do
     assert parse_timestamp("1544797214") == "1544797214"
-    assert parse_timestamp(1544797214) == "1544797214"
+    assert parse_timestamp(1_544_797_214) == "1544797214"
     assert parse_timestamp(1_544_797_214.955656) == "1544797214.955656"
     refute parse_timestamp(nil)
   end
@@ -13,13 +13,18 @@ defmodule Receivex.Parser.Test do
     assert parse_address("Bob <bob@mg.example.com>") == {"Bob", "bob@mg.example.com"}
     assert parse_address("bob@mg.example.com") == {"", "bob@mg.example.com"}
     refute parse_address("invalid email address")
-    refute parse_address(12345)
+    refute parse_address(12_345)
     refute parse_address(nil)
   end
 
   test "parse_recipients/1: returns list of email address tuples" do
     assert parse_recipients("Bob <bob@mg.example.com>") == [{"Bob", "bob@mg.example.com"}]
-    assert parse_recipients("Bob <bob@mg.example.com>, Sarah <sarah@mg.example.com>") == [{"Bob", "bob@mg.example.com"}, {"Sarah", "sarah@mg.example.com"}]
+
+    assert parse_recipients("Bob <bob@mg.example.com>, Sarah <sarah@mg.example.com>") == [
+             {"Bob", "bob@mg.example.com"},
+             {"Sarah", "sarah@mg.example.com"}
+           ]
+
     refute parse_recipients(nil)
   end
 end
