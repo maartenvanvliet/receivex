4d176517b40ec19f67748dd00b246150d221b53d
Chase Pursley <chasepursley@gmail.com>
2021-12-28 11:51:23 -0500

    Derp.

--- lib/receivex/handler.ex
+++ lib/receivex/handler.ex
@@ -4,15 +4,3 @@ defmodule Receivex.Handler do
   """
   @callback process(payload :: struct()) :: :ok | {:error, any()}
 end
-
-forward("_incoming",
-  to: Receivex,
-  init_opts: [
-    adapter: Receivex.Adapter.Mandrill,
-    adapter_opts: [
-      secret: "i8PTcm8glMgsfaWf75bS1FQ",
-      url: "http://example.com"
-    ],
-    handler: Example.Processor
-  ]
-)
